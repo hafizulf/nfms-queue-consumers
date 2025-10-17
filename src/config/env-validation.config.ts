0f34b7f9a2dddc6b6d4cd0212c92a2d51eaddbcf
Hafizul Furqan <hafizulf68@gmail.com>
2025-10-17 11:01:37 +0700

    chore: init app

---
+++ src/config/env-validation.config.ts
@@ -0,0 +1,23 @@
+import { z, ZodError } from 'zod';
+
+export const formatEnvErrors = (error: ZodError) => {
+  return error.issues.map((e) => ({
+    field: e.path.join('.'),
+    message: e.message,
+  }));
+}
+
+export const EnvValidationSchema = z.object({
+  APP_PORT: z
+    .string()
+    .regex(/^\d+$/, { message: 'PORT must be a number' })
+    .transform(Number),
+  RABBITMQ_URI: z.string(),
+});
+
+export const EnvValidationOptions = {
+  strict: true,
+  abortEarly: false,
+};
+
+export type Env = z.infer<typeof EnvValidationSchema>;
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+
+  const configService = app.get(ConfigService);
+  const appPort = configService.get('APP_PORT');
+
+  await app.listen(appPort);
+
+  console.log('Application is running on port:', appPort);
+}
+bootstrap();
